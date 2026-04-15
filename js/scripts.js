16def557fc079820cde4ab2d63dcfc44ea35021d
saobangmath <anhtai2672000@gmail.com>
2026-04-15 23:37:33 +0700

    refactor the website

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
-function onClick(){
-	var text = document.getElementById("demo").innerHTML;
-	if (text == 0){
-		text = 1;
-	}
-	else{
-		text = 0;
-	}
-	document.getElementById("demo").innerHTML = text;
+const yearElement = document.getElementById("year");
+
+if (yearElement) {
+  yearElement.textContent = new Date().getFullYear();
 }
